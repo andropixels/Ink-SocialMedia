eaa5ab213d53a87c66d4470ef4b6f4acf7e5d519
Rahul <rahul@riodefi.com>
2022-09-16 11:41:45 +0530

    newfeatures

--- dwitter/lib.rs
+++ dwitter/lib.rs
@@ -7,7 +7,7 @@ mod dwitter {
     // use ink_env::AccountId;
 
     use ink_storage::Mapping;
-    use scale::Encode;
+    use scale::{Encode, Decode};
 
 
     /// Defines the storage of your contract.
@@ -17,13 +17,18 @@ mod dwitter {
     pub struct Dwitter {
         /// Stores a single `bool` value on the storage.
         value: bool,
-        user:User
+        user:User,
+        totalDweet:i128,
+        dweets:Mapping<i128,Dweet>,
+        dweetLikers:Mapping<i128,AccountId>
     }
-    
 #[derive(Debug)]    
     
     enum accountStatus{NP,Active,Banned,Deactivated}
-    #[derive(Debug)]
+      
+    
+
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     enum cdStatus{NP,Active, Banned, Deleted}//Comment-Dweet status
 
     
@@ -57,6 +62,8 @@ mod dwitter {
     
     
     
+    #[derive(ink_storage::traits::SpreadAllocate, ink_storage::traits::SpreadLayout,ink_storage::traits::PackedLayout,Encode,Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub struct Dweet {
         dweetId:i128,
         author:AccountId,
@@ -66,16 +73,45 @@ mod dwitter {
         timestamp:i128,
         likecount:i128,
         reportCount:i128,
-        status:cdStatus
+        // status:cdStatus
 
     }
 
+    pub struct Comment {
+        commentId:i128,
+        author:AccountId,
+        dweetId:i128,
+        content:String,
+        likeCount:i128,
+        
+    }
+
+    #[ink(event)]
+    pub struct DweetCreated{
+        id:i128
+    }
+
+    #[ink(event)] 
+    pub struct DweetDeleted {
+        id:i128
+    }
+
+
+
+    #[ink(event)]
+    pub struct DweetLiked{
+        id:i128,
+        liker:AccountId
+    }
 
     impl Dwitter {
+
+        
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
         pub fn new(init_value: bool,username:String,name:String,bio:String) -> Self {
-            // let mut users = Mapping::default();
+             let mut dweets = Mapping::default();
+             let mut dweetLikers = Mapping::default();
             let caller = Self::env().caller();
 
             let user = User {
@@ -92,7 +128,7 @@ mod dwitter {
                 
             };
             // users.insert(caller, &user);
-            Self { value: init_value ,user}
+            Self { value: init_value ,user,totalDweet:0,dweets,dweetLikers:dweetLikers}
         }
 
         /// Constructor that initializes the `bool` value to `false`.
@@ -116,6 +152,63 @@ mod dwitter {
         pub fn get(&self) -> bool {
             self.value
         }
+
+
+        #[ink(message)]
+       pub fn createDweet(&self , hashtag:String,content:String,imghash:String) {
+              self.totalDweet.checked_add(1);
+              let id = self.totalDweet;
+              let caller = self.env().caller();
+            let  dweet = Dweet{
+                dweetId:id,
+                author:caller,
+                hashtag,
+                content,
+                imgHash:imghash,
+                timestamp:0,
+                likecount:0,
+                reportCount:0
+              };
+              self.dweets.insert(id, &dweet); 
+
+              self.env().emit_event( DweetCreated{ id });
+
+               
+       }
+
+       #[ink(message)]
+       pub fn deleteDweet(&self ,id:i128){
+           self.dweets.remove(id);
+           self.env().emit_event(DweetDeleted{id})
+
+       }
+   
+       #[ink(message)]
+       pub fn likeDweet(&self,id:i128){
+         let caller = self.env().caller(); 
+        let mut dweet =self.dweets.get(id).unwrap();
+        dweet.likecount.checked_add(1);
+        self.dweetLikers.insert(id, &caller);
+        self.env().emit_event(DweetLiked{id:id,liker:caller})
+
+
+       }
+
+       #[ink(message)]
+       pub fn create_comment(&self,id:i128,comment:String){
+        // let mut dweet = self.dweets.get(id).unwrap();
+        // dweet.c
+
+       }
+
+       #[ink(message)]
+       pub fn getDweet(&self,id:i128) -> Dweet {
+
+       return  self.dweets.get(id).unwrap();
+
+
+       }
+
     }
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
@@ -134,6 +227,7 @@ mod dwitter {
         fn default_works() {
             let dwitter = Dwitter::default();
             assert_eq!(dwitter.get(), false);
+      
         }
 
         /// We test a simple use case of our contract.
